3f19387a9aaad2d6e8a50c7cfd95756b2a9ec365
WojciechS <sankowski.wojciech@gmail.com>
2020-08-28 09:09:33 +0200

    fixed setting subdomain name in tutor component

--- src/app/tutor/component/tutor.component.ts
+++ src/app/tutor/component/tutor.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subdomain } from 'src/app/subdomain/model/subdomain.model';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/state/app.state';
+import { getSubdomainInformation } from 'src/app/subdomain/state/subdomain.select';
 
 @Component({
   selector: 'courses-tutor',
@@ -9,12 +13,12 @@ import { Subdomain } from 'src/app/subdomain/model/subdomain.model';
 })
 export class TutorComponent implements OnInit {
 
-  subdomainInformation: Subdomain;
+  subdomainInformation$: Observable<Subdomain>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private store: Store<State>) { }
 
   ngOnInit(): void {
-    this.subdomainInformation = this.route.snapshot.data.subdomainInformation;
+    const subdomainName = this.route.snapshot.params.subdomainName;
+    this.subdomainInformation$ = this.store.select(getSubdomainInformation);
   }
-
 }
